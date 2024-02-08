ca36e73e926f5b937b23812d0a5f5569611263a0
Leonardo Calheiros <leonardo.calheiros@gmail.com>
2024-02-08 14:37:32 -0300

    feat: implementing Authentication login dto's

---
+++ src/main/java/com/leocalheiros/paymentsystem/dto/AuthenticationRequest.java
@@ -0,0 +1,4 @@
+package com.leocalheiros.paymentsystem.dto;
+
+public record AuthenticationRequest(String email, String password) {
+}
---
+++ src/main/java/com/leocalheiros/paymentsystem/dto/AuthenticationResponse.java
@@ -0,0 +1,4 @@
+package com.leocalheiros.paymentsystem.dto;
+
+public record AuthenticationResponse(String token) {
+}
