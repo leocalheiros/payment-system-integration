849c629d6fc0046123d0040f157dc113b5466386
Leonardo Calheiros <leonardo.calheiros@gmail.com>
2024-02-08 14:27:54 -0300

    feat: add User entity

---
+++ src/main/java/com/leocalheiros/paymentsystem/entity/User.java
@@ -0,0 +1,86 @@
+package com.leocalheiros.paymentsystem.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Getter
+@Setter
+@Entity(name = "users")
+@Table(name = "users")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    private String verificationCode;
+
+    private boolean enabled;
+
+    private String role;
+
+    public User(Long id, String name, String email, String password, String verificationCode, boolean enabled, String role) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.verificationCode = verificationCode;
+        this.enabled = enabled;
+        this.role = role;
+    }
+
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User(){}
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.enabled;
+    }
+
+
+}
