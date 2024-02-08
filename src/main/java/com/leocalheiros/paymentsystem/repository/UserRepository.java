455c5707476c1c01041ee2b310197798204e04e1
Leonardo Calheiros <leonardo.calheiros@gmail.com>
2024-02-08 14:34:18 -0300

    feat: implementing UserRepository

---
+++ src/main/java/com/leocalheiros/paymentsystem/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.leocalheiros.paymentsystem.repository;
+
+import com.leocalheiros.paymentsystem.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    UserDetails findByEmail(String email);
+    User findByVerificationCode(String verificationCode);
+}
